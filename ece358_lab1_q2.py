15d79ecefa89d5f2523276c5e47a843d9ee42b93
Harry Ryu <Kryu@uwaterloo.ca>
2021-10-04 19:47:27 -0400

    Update

--- ece358_lab1_q2.py
+++ ece358_lab1_q2.py
@@ -1,27 +1,57 @@
 import math
 import random
+import time
 
 def exponential_random_var(lambd):
     uniform_random_var = random.random()
     return -1 * (1/lambd) * math.log(1 - uniform_random_var)
 
 class Queue:
-    def __init__(self, simulation_time, lambd):
-        self.lambd = lambd
+    def __init__(self, simulation_time, packet_rate_lambd, size_lambd, transmission_rate):
+        self.packet_rate_lambd = packet_rate_lambd
+        self.size_lambd = size_lambd
         self.simulation_time = simulation_time
 
         simulation_duration = 0
+        departure_time = 0
         queue = []
 
         while (1):
-            time = exponential_random_var(lambd)
-            packet_size = exponential_random_var(lambd)
+            time = exponential_random_var(packet_rate_lambd)
+            packet_size = exponential_random_var(size_lambd)
 
             simulation_duration += time
-            queue.append(Event(packet_size, simulation_duration))
+            queue.append(Event("Arrival", simulation_duration))
+
+            service_time = packet_size / transmission_rate
+
+            if (simulation_duration < departure_time):
+                departure_time = departure_time + service_time
+            else:
+                departure_time = simulation_duration + service_time
+        
+            if (departure_time < simulation_time):
+                queue.append(Event("Departure", departure_time))
+
 
             if (simulation_duration > simulation_time):
                 break
+        
+        simulation_duration = 0
+        while (1):
+            time = exponential_random_var(packet_rate_lambd * 5)
+            simulation_duration += time
+            queue.append(Event("Observer", simulation_duration))
+
+            if (simulation_duration > simulation_time):
+                break
+
+
+        queue.sort(key=Event.get_occurrence_time)
+        
+        # for item in queue:
+        #     print(item.get_event_type())
+        #     print(item.get_occurrence_time())
 
         self.queue = queue
 
@@ -31,55 +61,43 @@ class Queue:
     def size(self):
         return len(self.queue)
 
-
 class Event:
-    def __init__(self, packet_size, arrive_time):
-        self.packet_size = packet_size
-        self.arrive_time = arrive_time
+    def __init__(self, event_type, occurrence_time):
+        self.event_type = event_type
+        self.occurrence_time = occurrence_time
     
-    def get_arrival_time(self):
-        return self.arrive_time
+    def get_event_type(self):
+        return self.event_type
 
-    def get_packet_size(self):
-        return self.packet_size
+    def get_occurrence_time(self):
+        return self.occurrence_time
 
     def insert_service_time(self, service_time):
         self.service_time = service_time
     
     def insert_departure_time(self, departure_time):
-        self.departure_time = departure_time
-
-class EventList:
-    def __init__(self, arrival_time, packet_size, service_time, departure_time):
-        self.packet_size = packet_size
-        self.arrive_time = arrival_time
-        self.service_time = service_time
-        self.departure_time = departure_time
+        self.departure_time = departure_time        
 
 
-def calculateDeparture(arrivalEvent, transmission_rate):
-    service_time = arrivalEvent.get_packet_size() / transmission_rate
-    departure_time = arrivalEvent.get_arrival_time() + service_time
-    arrivalEvent.insert_service_time(service_time)
-    arrivalEvent.insert_departure_time(departure_time)
-
-        
-
-
-def startSimulation(simulation_time, lambd, transmission_rate):
-    queue = Queue(simulation_time, lambd)
+def startSimulation(simulation_duration, packet_rate_lambd, size_lambd, transmission_rate):
+    queue = Queue(simulation_duration, packet_rate_lambd, size_lambd, transmission_rate)
 
     output_list = []
 
-    while (queue.size != 0):
-        arrival_event = queue.output_queue()
-        
-        calculateDeparture(arrival_event, transmission_rate)
+    packets_in_buffer = 0
 
-        output_list.append(arrival_event)
-    
+    while (queue.size() != 0):
+        event = queue.output_queue()
+
+        if (event.get_event_type() == "Arrival"):
+            packets_in_buffer += 1
+        elif (event.get_event_type() == "Departure"):
+            packets_in_buffer -= 1
+        elif (event.get_event_type() == "Observer"):
+            output_list.append({'time': event.get_occurrence_time(), 'packets': packets_in_buffer})
+
     print(output_list)
 
-startSimulation(10, 50, 0.01)
+startSimulation(10, 50, 1/2000, 1000000)
 
 
