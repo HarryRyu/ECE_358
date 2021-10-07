163ee63b051045e99b3d22af8dff5e51ae5ff5f5
kevan26 <k4an@uwaterloo.ca>
2021-10-06 20:35:06 -0400

    More comments and clean up of q3

--- ece358_lab1_q2.py
+++ ece358_lab1_q2.py
@@ -68,7 +68,7 @@ class Queue:
 
     def remove_departure(self, link_number):
         for i in reversed(range(len(self.queue))):
-            if (self.queue[i].get_link_number() == link_number):
+            if self.queue[i].get_link_number() == link_number:
                 self.queue[i].set_event_type("Deprecated")
                 return
 
@@ -92,10 +92,10 @@ class Event:
         self.service_time = service_time
 
     def insert_departure_time(self, departure_time):
-        self.departure_time = departure_time     
+        self.departure_time = departure_time
 
     def get_link_number(self):
-        return self.link_number 
+        return self.link_number
 
     def set_event_type(self, event):
         self.event_type = event
@@ -105,97 +105,104 @@ def startSimulation(simulation_duration, packet_rate_lambd, size_lambd, transmis
     queue = Queue(simulation_duration, packet_rate_lambd, size_lambd, transmission_rate)
 
     # Define variables and assign starting values.
-    number_of_packets = 0
-    no_packets = 0
-    packets_in_buffer = 0
-    packets_loss = 0
-    observer_count = 0
-    total_packets_arrival = 0
+    number_of_packets = 0  # Used for counting how many packets in buffer.
+    no_packets = 0  # Used for counting how many times there are no packets in buffer.
+    packets_in_buffer = 0  # Used for counting total number of packets in buffer.
+    packets_loss = 0  # Used for counting if packets are dropped due to buffer overflow.
+    observer_count = 0  # Used for counting total number of observer events.
+    total_packets_arrival = 0  # Used for counting total number of arrived packets.
     i = 0
     queue_size = queue.size()
-    while (queue.size() != 0):
-        
+    while queue.size() != 0:
+
         percentage = math.floor((i / queue_size * 100))
-        if ( percentage % 10 == 0):
+        if percentage % 10 == 0:
             print(f'{percentage}% done')
             sys.stdout.write("\033[F")
 
         event = queue.output_queue()
 
-        if (event.get_event_type() == "Arrival"):
-            if ((buffer_size >= 0) and (packets_in_buffer + 1 >= buffer_size)):
+        # Depending on the type of the event, modify the buffer and increment
+        # respective counters.
+        if event.get_event_type() == "Arrival":
+            # Buffer is full and need to drop arrival and respective departure packet.
+            if (buffer_size >= 0) and (packets_in_buffer + 1 >= buffer_size):
                 link_number = event.get_link_number()
                 queue.remove_departure(link_number)
                 packets_loss += 1
             else:
                 packets_in_buffer += 1
             total_packets_arrival += 1
-        elif (event.get_event_type() == "Departure"):
+
+        elif event.get_event_type() == "Departure":
             packets_in_buffer -= 1
-        elif (event.get_event_type() == "Observer"):
+
+        elif event.get_event_type() == "Observer":
             observer_count += 1
             number_of_packets += packets_in_buffer
-            if (packets_in_buffer == 0):
+            if packets_in_buffer == 0:
                 no_packets += 1
         i += 1
 
-
-
+    # Calculate performance metrics.
     packets_average = number_of_packets / observer_count
     packets_idle = no_packets / observer_count
     packets_loss = packets_loss / total_packets_arrival
 
-    return {"packets_average": packets_average, "packets_idle" : packets_idle, "packets_loss" : packets_loss}
+    return {"packets_average": packets_average, "packets_idle": packets_idle, "packets_loss": packets_loss}
+
 
 # For question 3
 def simulate_question_3():
     start_time = time.time()
-    # Constant variables
+
+    # Define constant and assign variables.
     TRANSMISSION_TIME = 1000000
     PACKET_SIZE = 2000
-
-
     simulation_time = 0
 
-    # List to store # of packets in buffer
+    # List to store # of packets in buffer.
     prev_average_list_y = []
-    # List to store percentage of idle time for buffer
+
+    # List to store percentage of idle time for buffer.
     prev_idle_list_y = []
-    # List of p steps
+
+    # List of p steps.
     list_x = []
-    # For stability testing
+
+    # For stability testing.
     test_pass = False
 
     # Run until margin is less than 5%
-    while (not test_pass):
+    while not test_pass:
         current_time = time.time() - start_time
         minutes = math.floor(current_time / 60)
         seconds = current_time % 60
-        
+
         print(f'It has been {minutes}m {seconds}s')
         simulation_time += 1000
         print(f'Running simulation with time: {simulation_time}')
         # Run with different values of p, step size of 0.1
-        for i in range(25, 95, 10):
-            print(f'Initiating row of: {i}')
+        for i in range(25, 35, 10):
+            print(f'Initiating rho of: {i/100}')
             average_list_y = []
             idle_list_y = []
             p = i * TRANSMISSION_TIME / 2000 / 100
-            simulation_result = startSimulation(simulation_time, p , 1 / PACKET_SIZE, TRANSMISSION_TIME, 10)
+            simulation_result = startSimulation(simulation_time, p, 1 / PACKET_SIZE, TRANSMISSION_TIME, 10)
             average_list_y.append(simulation_result["packets_average"])
             idle_list_y.append(simulation_result["packets_idle"])
             list_x.append(i / 100)
 
-        if (len(prev_average_list_y) != 0):
+        if len(prev_average_list_y) != 0:
             for i in range(len(prev_average_list_y)):
-                margin = math.abs((prev_average_list_y[i] - average_list_y[i]) / prev_average_list_y[i])
-                margin2 = math.abs((prev_idle_list_y[i] - idle_list_y[i]) / prev_idle_list_y[i])
-                if (margin > 0.05 and margin2 > 0.05):
+                margin = abs((prev_average_list_y[i] - average_list_y[i]) / prev_average_list_y[i])
+                margin2 = abs((prev_idle_list_y[i] - idle_list_y[i]) / prev_idle_list_y[i])
+                if margin > 0.05 and margin2 > 0.05:
                     test_pass = False
                     break
                 else:
                     test_pass = True
-        
+
         prev_average_list_y = average_list_y
         prev_idle_list_y = idle_list_y
 
@@ -212,14 +219,16 @@ def simulate_question_3():
     plt.title(f'P_idle vs Utilization of Queue with Simulation Time: {simulation_time}')
     plt.show()
 
+
 # Constant variables
 TRANSMISSION_TIME = 1000000
 PACKET_SIZE = 2000
 
 # List to store # of packets in buffer
-final_average_list_y = [[],[],[]]
+final_average_list_y = [[], [], []]
 # List to store percentage of idle time for buffer
-final_loss_list_y = [[],[],[]]
+final_loss_list_y = [[], [], []]
+
 
 # For question 6
 def simulate_question_6():
@@ -227,7 +236,7 @@ def simulate_question_6():
     start_time = time.time()
     packet_rate_init = 50
     packet_rate_end = 150
-    buffer_size = [10,25,50]
+    buffer_size = [10, 25, 50]
 
     # List of p steps
     list_x = []
@@ -237,7 +246,8 @@ def simulate_question_6():
 
     thread_list = []
     for j in range(len(buffer_size)):
-        thread_list.append(threading.Thread(target=simlutate_finite_buffer, args=(j, buffer_size[j], packet_rate_init, packet_rate_end)))
+        thread_list.append(threading.Thread(target=simlutate_finite_buffer,
+                                            args=(j, buffer_size[j], packet_rate_init, packet_rate_end)))
 
     for thread in thread_list:
         thread.start()
@@ -274,7 +284,8 @@ def simulate_question_6():
 def simlutate_finite_buffer(j, buffer_size, packet_rate_init, packet_rate_end):
     print(f'Finite Buffer Thread with Buffer Size {buffer_size} Started')
     simulation_start_constant = 500
-    # List for storing output. It will be a 3D list: 2nd dimension for storing different simulation time, 3rd dimension for storing different packet rates
+    # List for storing output. It will be a 3D list: 2nd dimension for storing different simulation time,
+    # 3rd dimension for storing different packet rates.
     average_list_y = [[], []]
     loss_list_y = [[], []]
     for packet_rate in range(packet_rate_init, packet_rate_end, 10):
@@ -282,7 +293,7 @@ def simlutate_finite_buffer(j, buffer_size, packet_rate_init, packet_rate_end):
         average_list_y[1].append([])
         loss_list_y[0].append([])
         loss_list_y[1].append([])
-    list_ptr = 0;
+    list_ptr = 0
 
     # Increment simulation_time until margin of output is less than 5%
     simulation_time = simulation_start_constant
@@ -293,7 +304,9 @@ def simlutate_finite_buffer(j, buffer_size, packet_rate_init, packet_rate_end):
     list_counter = 0
     thread_list = []
     for packet_rate in range(packet_rate_init, packet_rate_end, 10):
-        thread_list.append(threading.Thread(target=simulate_thread, args=(simulation_time, packet_rate, buffer_size, average_list_y[0][list_counter], [], loss_list_y[0][list_counter])))
+        thread_list.append(threading.Thread(target=simulate_thread, args=(
+            simulation_time, packet_rate, buffer_size, average_list_y[0][list_counter], [],
+            loss_list_y[0][list_counter])))
         list_counter += 1
 
     # Start running the threads
@@ -305,16 +318,19 @@ def simlutate_finite_buffer(j, buffer_size, packet_rate_init, packet_rate_end):
     # Stop the function if test passes. Test will pass if the margin is less than 5%
     test_pass = False
 
-    while (not test_pass):
-        # Increment the simulation time by 1000s. It should start at 2000s and continue until margin requirements are met
+    while not test_pass:
+        # Increment the simulation time by 1000s.
+        # It should start at 2000s and continue until margin requirements are met.
         simulation_time += simulation_start_constant
         list_ptr = list_ptr ^ 1
 
-        # Create threads to run simulation at different packet rates
+        # Create threads to run simulation at different packet rates.
         list_counter = 0
         thread_list_next = []
         for packet_rate in range(packet_rate_init, packet_rate_end, 10):
-            thread_list_next.append(threading.Thread(target=simulate_thread, args=(simulation_time, packet_rate, buffer_size, average_list_y[list_ptr][list_counter], [], loss_list_y[list_ptr][list_counter])))
+            thread_list_next.append(threading.Thread(target=simulate_thread, args=(
+                simulation_time, packet_rate, buffer_size, average_list_y[list_ptr][list_counter], [],
+                loss_list_y[list_ptr][list_counter])))
             list_counter += 1
 
         # Start running the threads
@@ -322,7 +338,7 @@ def simlutate_finite_buffer(j, buffer_size, packet_rate_init, packet_rate_end):
             thread.start()
 
         # Wait for the threads to stop running
-        if (simulation_time == simulation_start_constant * 2):
+        if simulation_time == simulation_start_constant * 2:
             for thread in thread_list:
                 thread.join()
 
@@ -333,11 +349,11 @@ def simlutate_finite_buffer(j, buffer_size, packet_rate_init, packet_rate_end):
         for i in range(len(average_list_y[0])):
             # Margin value of average # of packet in buffer
             margin_packet = abs((average_list_y[0][i][0] - average_list_y[1][i][0]) / average_list_y[0][i][0])
-            if ((loss_list_y[0][i][0]) != 0):
+            if (loss_list_y[0][i][0]) != 0:
                 margin_loss = abs((loss_list_y[0][i][0] - loss_list_y[1][i][0]) / loss_list_y[0][i][0])
             else:
                 margin_loss = abs((loss_list_y[0][i][0] - loss_list_y[1][i][0]))
-            if (margin_packet > 0.05 or margin_loss > 0.05):
+            if margin_packet > 0.05 or margin_loss > 0.05:
                 test_pass = False
                 break
             else:
@@ -349,13 +365,13 @@ def simlutate_finite_buffer(j, buffer_size, packet_rate_init, packet_rate_end):
 
     # Store data into file as backup
     f = open(f'ECE318_Q6_Buffer_Size_{buffer_size}_Average_Output', "w")
-    str = repr(average_list_y[list_ptr])
-    f.write(str)
+    string1 = repr(average_list_y[list_ptr])
+    f.write(string1)
     f.close()
 
     f = open(f'ECE318_Q6_Buffer_Size_{buffer_size}_Loss_Output', "w")
-    str = repr(loss_list_y[list_ptr])
-    f.write(str)
+    string1 = repr(loss_list_y[list_ptr])
+    f.write(string1)
     f.close()
 
     print(f'Finite Buffer Thread with Buffer Size {buffer_size} ended')
@@ -363,20 +379,26 @@ def simlutate_finite_buffer(j, buffer_size, packet_rate_init, packet_rate_end):
 
 # Each function runs a simulation
 def simulate_thread(simulation_time, packet_rate, buffer_size, average_list_y, idle_list_y, loss_list_y):
-    print(f'Simulation with packet rate {packet_rate} and buffer size {buffer_size} and simulation time {simulation_time} started')
-    # Calculate packet_rate
+    print(
+        f'Simulation with packet rate {packet_rate} and buffer size {buffer_size} '
+        f'and simulation time {simulation_time} started')
+    # Calculate packet_rate.
     p = packet_rate * TRANSMISSION_TIME / 2000 / 100
-    # Run the simulation
-    simulation_result = startSimulation(simulation_time, p , packet_rate / PACKET_SIZE, TRANSMISSION_TIME, buffer_size)
-    # Store into central list
+
+    # Run the simulation.
+    simulation_result = startSimulation(simulation_time, p, packet_rate / PACKET_SIZE, TRANSMISSION_TIME, buffer_size)
+
+    # Store into central list.
     average_list_y.append(simulation_result["packets_average"])
     idle_list_y.append(simulation_result["packets_idle"])
     loss_list_y.append(simulation_result["packets_loss"])
 
-    print(f'Simulation with packet rate {packet_rate} and buffer size {buffer_size} and simulation time {simulation_time} ended')
+    print(f'Simulation with packet rate {packet_rate} and buffer size {buffer_size} '
+          f'and simulation time {simulation_time} ended')
 
 # current_time = time.time() - start_time
 # minutes = math.floor(current_time / 60)
 # seconds = current_time % 60
 
 # print(f'It has been {minutes}m {seconds}s')
+simulate_question_3()
